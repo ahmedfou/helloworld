08481d311779c9f5c394aa1ccab2987ba00f7531
ahmedfou <ahmedfou2014@gmail.com>
2025-02-12 16:46:44 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("hello world jenkinsFile");
-    System.out.println("hello world after commit&push ");
+    System.out.println("hello world after commit&push");
   }
 }
